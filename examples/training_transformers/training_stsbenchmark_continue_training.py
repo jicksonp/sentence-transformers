e266bbe14e4dcd4d5c295003130779627c42b5ba
Jickson P <jicksonstephen@gmail.com>
2020-07-31 21:21:03 +0530

    change model name

--- examples/training_transformers/training_stsbenchmark_continue_training.py
+++ examples/training_transformers/training_stsbenchmark_continue_training.py
@@ -7,12 +7,11 @@ If you want to fine-tune a huggingface/transformers model like bert-base-uncased
 """
 from torch.utils.data import DataLoader
 import math
-from sentence_transformers import SentenceTransformer,  SentencesDataset, LoggingHandler, losses
+from sentence_transformers import SentenceTransformer, SentencesDataset, LoggingHandler, losses
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 from sentence_transformers.readers import STSBenchmarkDataReader
 import logging
 from datetime import datetime
-
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -22,10 +21,11 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 #### /print debug information to stdout
 
 # Read the dataset
-model_name = 'bert-large-nli-mean-tokens'
+model_name = 'bert-base-nli-stsb-mean-tokens'
 train_batch_size = 16
 num_epochs = 4
-model_save_path = 'output/training_stsbenchmark_continue_training-'+model_name+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+model_save_path = 'output/training_stsbenchmark_continue_training-' + model_name + '-' + datetime.now().strftime(
+    "%Y-%m-%d_%H-%M-%S")
 sts_reader = STSBenchmarkDataReader('../datasets/stsbenchmark', normalize_scores=True)
 
 # Load a pre-trained sentence transformer model
@@ -37,17 +37,14 @@ train_dataset = SentencesDataset(sts_reader.get_examples('sts-train.csv'), model
 train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=train_batch_size)
 train_loss = losses.CosineSimilarityLoss(model=model)
 
-
 logging.info("Read STSbenchmark dev dataset")
 dev_data = SentencesDataset(examples=sts_reader.get_examples('sts-dev.csv'), model=model)
 dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=train_batch_size)
 evaluator = EmbeddingSimilarityEvaluator(dev_dataloader)
 
-
 # Configure the training. We skip evaluation in this example
-warmup_steps = math.ceil(len(train_dataset) * num_epochs / train_batch_size * 0.1) #10% of train data for warm-up
+warmup_steps = math.ceil(len(train_dataset) * num_epochs / train_batch_size * 0.1)  # 10% of train data for warm-up
 logging.info("Warmup-steps: {}".format(warmup_steps))
-
 
 # Train the model
 model.fit(train_objectives=[(train_dataloader, train_loss)],
@@ -56,7 +53,6 @@ model.fit(train_objectives=[(train_dataloader, train_loss)],
           evaluation_steps=1000,
           warmup_steps=warmup_steps,
           output_path=model_save_path)
-
 
 ##############################################################################
 #
